4afffb11e11ff9f2ac5c8adfa8d1772a2fe5cbf8
Emily Pearl <Emilypearl9193@gmail.com>
2024-01-15 00:47:25 -0700

    placeholder  project card components

---
+++ src/lib/project-cards/Cards.svelte
@@ -0,0 +1,26 @@
+<section class="m-auto flex flex-row justify-evenly p-4">
+  <section class=" max-w-72 max-h-96 bg-zinc-400 rounded-2xl">
+    <div class="w-60 h-40 bg-black m-auto mt-4"></div>
+    <p class="text-lg text-black p-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+      reprehenderit nihil quidem impedit assumenda hic voluptatem aut
+      laudantium.
+    </p>
+  </section>
+  <section class=" max-w-72 max-h-96 bg-zinc-400 rounded-2xl">
+    <div class="w-60 h-40 bg-black m-auto mt-4"></div>
+    <p class="text-lg text-black p-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+      reprehenderit nihil quidem impedit assumenda hic voluptatem aut
+      laudantium.
+    </p>
+  </section>
+  <section class=" max-w-72 max-h-96 bg-zinc-400 rounded-2xl">
+    <div class="w-60 h-40 bg-black m-auto mt-4"></div>
+    <p class="text-lg text-black p-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+      reprehenderit nihil quidem impedit assumenda hic voluptatem aut
+      laudantium.
+    </p>
+  </section>
+</section>
