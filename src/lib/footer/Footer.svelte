f6ae324892141da88a6a9a4058fc047dd213291e
Emily Pearl <Emilypearl9193@gmail.com>
2024-01-15 00:48:26 -0700

    added cards to page and fixed spelling issues in about me description

--- src/lib/footer/Footer.svelte
+++ src/lib/footer/Footer.svelte
@@ -1,21 +1,31 @@
 <footer>
   <section class="m-2 text-center flex flex-row justify-evenly">
     <ul class="flex flex-col">
-      <li class="text-pink-500 text-lg p-1">
+      <li class="text-pink-500 text-lg text-left p-1">
         <a href="https://www.linkedin.com/in/emilyseafoot/">LinkedIn</a>
       </li>
-      <li class="text-pink-500 text-lg p-1">
+      <li class="text-pink-500 text-lg text-left p-1">
         <a href="https://github.com/Emilypearl91">Github</a>
       </li>
-      <li class="text-pink-500 text-lg p-1"><a href="/">Resume</a></li>
+      <li class="text-pink-500 text-lg text-left p-1">
+        <a href="/">Resume</a>
+      </li>
     </ul>
     <ul class="m-2 text-center flex flex-col">
-      <li class="text-pink-500 text-lg p-1"><a href="/">Project one</a></li>
-      <li class="text-pink-500 text-lg p-1"><a href="/">Project Two</a></li>
-      <li class="text-pink-500 text-lg p-1"><a href="/">Project Three</a></li>
+      <li class="text-pink-500 text-lg text-left p-1">
+        <a href="/">Project one</a>
+      </li>
+      <li class="text-pink-500 text-lg text-left p-1">
+        <a href="/">Project Two</a>
+      </li>
+      <li class="text-pink-500 text-lg text-left p-1">
+        <a href="/">Project Three</a>
+      </li>
     </ul>
     <ul class="m-2 text-center flex flex-col">
-      <li class="text-pink-500 text-lg p-1">Emilypearl9193@gmail.com</li>
+      <li class="text-pink-500 text-lg text-left p-1">
+        Emilypearl9193@gmail.com
+      </li>
     </ul>
   </section>
   <h4 class="text-white text-center m-2 text-lg">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,18 @@
+<script>
+  import Cards from "../lib/project-cards/Cards.svelte";
+</script>
+
 <body>
   <h1 class="text-pink-500 text-center text-4xl">About Me</h1>
   <section class="m-auto flex flex-row">
     <!---place holder for image --->
     <section class="w-60 h-60 rounded-full bg-zinc-400 m-auto"></section>
     <p class="max-w-4xl m-auto text-xl">
-      Hello! My name is Emily and i am from calgary alberta. My background in
+      Hello! My name is Emily and I am from Calgary, Alberta. My background in
       the arts is how I found a passion for coding. I have been a classically
-      trained flutist for the past 10 years while in highschool I graduated with
-      a certificate of excellence in the arts, from there I chose to branch out
-      and try something new. I went into digital design where I learned to
+      trained flutist for the past 10 years. While in highschool I graduated
+      with a certificate of excellence in the arts, from there I chose to branch
+      out and try something new. I went into digital design where I learned to
       design branding, logos, website design mockups, wireframes and write
       copywriting for brands. Through that I got to see the beginning steps in
       creating and coding a portfolio website using html, css and javascript, I
@@ -17,6 +21,7 @@
     </p>
   </section>
   <h1 class="text-pink-500 text-center text-4xl">Projects</h1>
+  <Cards></Cards>
 </body>
 
 <style>
