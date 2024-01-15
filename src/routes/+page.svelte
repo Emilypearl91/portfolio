95a016fb966498c1d7886cbbf6c93daa862f8609
Emily Pearl <Emilypearl9193@gmail.com>
2024-01-14 23:46:01 -0700

    about me description and img placeholder bubble

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,29 @@
 <body>
   <h1 class="text-pink-500 text-center text-4xl">About Me</h1>
+  <section class="m-auto flex flex-row">
+    <!---place holder for image --->
+    <section class="w-60 h-60 rounded-full bg-zinc-400 m-auto"></section>
+    <p class="max-w-4xl m-auto text-xl">
+      Hello! My name is Emily and i am from calgary alberta. My background in
+      the arts is how I found a passion for coding. I have been a classically
+      trained flutist for the past 10 years while in highschool I graduated with
+      a certificate of excellence in the arts, from there I chose to branch out
+      and try something new. I went into digital design where I learned to
+      design branding, logos, website design mockups, wireframes and write
+      copywriting for brands. Through that I got to see the beginning steps in
+      creating and coding a portfolio website using html, css and javascript, I
+      really enjoyed the whole process and coding and chose to pursue that
+      passion further at SAIT in the web developer program.
+    </p>
+  </section>
   <h1 class="text-pink-500 text-center text-4xl">Projects</h1>
 </body>
 
 <style>
   :global(body) {
     background-color: black;
+    color: white;
+
     height: 100vh;
     padding: 0;
     margin: 0;
