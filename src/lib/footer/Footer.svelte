803fefd70779054bb5043ce006f43c901dab3eea
Emily Pearl <Emilypearl9193@gmail.com>
2024-10-10 18:50:08 -0600

    changed footer text color

--- src/lib/footer/Footer.svelte
+++ src/lib/footer/Footer.svelte
@@ -1,35 +1,33 @@
 <footer>
   <section class="m-2 text-center flex flex-row justify-evenly">
     <ul class="flex flex-col">
-      <li class="text-pink-500 text-lg text-left p-1">
+      <li class="text-black text-lg text-left p-1">
         <a href="https://www.linkedin.com/in/emilyseafoot/">LinkedIn</a>
       </li>
-      <li class="text-pink-500 text-lg text-left p-1">
+      <li class="text-black text-lg text-left p-1">
         <a href="https://github.com/Emilypearl91">Github</a>
       </li>
-      <li class="text-pink-500 text-lg text-left p-1">
+      <li class="text-black text-lg text-left p-1">
         <a href="/Resume">Resume</a>
       </li>
     </ul>
     <ul class="m-2 text-center flex flex-col">
-      <li class="text-pink-500 text-lg text-left p-1">
+      <li class="text-black text-lg text-left p-1">
         <a href="https://yarn-project-helper.netlify.app/"
           >Yarn Project Helper</a
         >
       </li>
-      <li class="text-pink-500 text-lg text-left p-1">
+      <li class="text-black text-lg text-left p-1">
         <a href="https://tossed.netlify.app/">Tossed</a>
       </li>
-      <li class="text-pink-500 text-lg text-left p-1">
+      <li class="text-black text-lg text-left p-1">
         <a href="https://emilypearl91.github.io/book-reading-prompt/"
           >Book Prompt Generator</a
         >
       </li>
     </ul>
     <ul class="m-2 text-center flex flex-col">
-      <li class="text-pink-500 text-lg text-left p-1">
-        Emilypearl9193@gmail.com
-      </li>
+      <li class="text-black text-lg text-left p-1">Emilypearl9193@gmail.com</li>
     </ul>
   </section>
   <h4 class="text-white text-center m-2 text-lg">
