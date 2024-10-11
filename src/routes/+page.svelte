893fa3461005cea1b1c845fc24210a7ba929acbf
Emily Pearl <Emilypearl9193@gmail.com>
2024-10-10 18:53:58 -0600

    changed font size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,8 @@
   class=" bg-gradient-to-t bg-[#CDCDCD] from-[#AA4465] rounded-md
 "
 >
-  <h1 id="projects" class="m-auto text-center text-xl">Projects</h1>
-  <p class="max-w-[80ch] m-auto">
+  <h1 id="projects" class="m-auto text-center text-2xl">Projects</h1>
+  <p class="max-w-[80ch] m-auto text-xl">
     The projects showcased are ones that I am most proud of and are currently
     working on. I started learning web development a year ago and since then I
     have been learning something new everyday that I can implement into my
@@ -24,7 +24,7 @@
       /></a
     >
     <div class="max-w-[500px] h-96 m-2 bg-[#DCC9AF] rounded-md one m-auto">
-      <p class=" m-auto max-w-[60ch] p-2">
+      <p class=" m-auto max-w-[60ch] p-2 text-xl">
         The yarn project helper was created with friend and family in mind, it
         is meant to replace the random sheets of paper and notebooks filled with
         tally marks to mark where you are in a fiber art projects. Allowing for
@@ -49,7 +49,7 @@
       /></a
     >
     <div class="max-w-[500px] h-98 m-2 bg-[#DCC9AF] rounded-md two m-auto">
-      <p class=" m-auto p-2">
+      <p class=" m-auto p-2 text-xl">
         The blog project was created to work with supabase and svelte by
         creating a website that renders data from supabase. It was also created
         to share knowledge, opinions and advice on the hobbies I share with
@@ -60,7 +60,7 @@
 
     <div class="max-w-[500px] h-64 m-2 bg-[#DCC9AF] rounded-md three">
       <p
-        class="text-center text-center m-24
+        class="text-center text-center m-24 text-xl
       "
       >
         coming soon...
@@ -70,7 +70,7 @@
     <div
       class="max-w-[500px] h-64 m-2 bg-[#DCC9AF] rounded-md three opacity-0 hover:opacity-100 hover:shadow hover:shadow-black hover:shadow-lg"
     >
-      <p class=" m-auto p-2">
+      <p class=" m-auto p-2 text-xl">
         A project that is currently in the works is getting the burger if the
         day from the bob's burger api and rendering it to be on a sheet of
         paper. Another is a book journal project that uses a database and user
@@ -81,13 +81,13 @@
       </p>
     </div>
   </section>
-  <h1 id="about" class="m-auto text-center text-xl">About me</h1>
+  <h1 id="about" class="m-auto text-center text-2xl">About me</h1>
   <img
     src="/photo-of-me.JPG"
     alt="photo of emily"
     class="max-w-52 max-h-52 rounded-full m-auto"
   />
-  <p class="max-w-[80ch] m-auto">
+  <p class="max-w-[80ch] m-auto p-4 text-xl">
     Hello! My name is Emily and I am from Calgary, Alberta. My background in the
     arts is how I found a passion for coding. I have been a classically trained
     flutist for the past 10 years. While in highschool I graduated with a
@@ -104,7 +104,6 @@
 <style>
   body {
     font-family: "Newsreader", serif;
-    font-size: 20px;
     font-weight: 500;
     font-style: normal;
   }
