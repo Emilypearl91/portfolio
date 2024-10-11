5ab6e1c2564171db8135276db9fe7bfe7ca33e15
Emily Pearl <Emilypearl9193@gmail.com>
2024-10-10 19:04:16 -0600

    removed navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,7 @@
 <script>
   import "../app.css";
   import Footer from "../lib/footer/Footer.svelte";
-  import Navbar from "../lib/Nav/Navbar.svelte";
 </script>
 
-<!-- <Navbar></Navbar> -->
-<Navbar></Navbar>
 <slot />
 <Footer></Footer>
