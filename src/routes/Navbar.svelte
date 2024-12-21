3ac5d4938d0367cdad336d3ddbe87d5a62530407
Emily Pearl <Emilypearl9193@gmail.com>
2024-12-20 20:18:53 -0700

    navbar

---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,10 @@
+<section class="h-24 bg-secondary">
+  <h1 class="text-3xl absolute m-6 font-serif">Emily Seafoot</h1>
+  <div class="">
+    <ul  class="flex flex-row justify-end">
+      <a href="/" class="hover:text-primary"><li class="m-6 text-xl">Projects</li></a>
+      <a href="/" class="hover:text-primary"><li class="m-6 text-xl">Resume</li></a>
+      <a href="/" class="hover:text-primary"><li class="m-6 text-xl">About Me</li></a>
+    </ul>
+  </div>
+</section>
